5dbeb32ab0499d098d736f0aa8e7702ef07ed525
razscalelot <you@example.com>
2023-10-02 11:57:10 +0530

    service edit

--- routes/back/dashboard.js
+++ routes/back/dashboard.js
@@ -5,12 +5,14 @@ const constants = require('../../utilities/constants');
 const contactusModel = require('../../models/contactus.model');
 const offerModel = require('../../models/offers.model');
 const quoteModel = require('../../models/quotes.model');
+const serviceModel = require('../../models/services.model');
 /* GET home page. */
 router.get('/', async (req, res) => {
     const token = req.cookies.token;
     if (token) {
         let primary = mongoConnection.useDb(constants.DEFAULT_DB);
         let contactusCount = await primary.model(constants.MODELS.contactus, contactusModel).countDocuments({ _id: { $ne: null } })
+        let serviceCount = await primary.model(constants.MODELS.services, serviceModel).countDocuments({ _id: { $ne: null } })
         let offerCount = await primary.model(constants.MODELS.offers, offerModel).countDocuments({ _id: { $ne: null } })
         let quoteCount = await primary.model(constants.MODELS.quotes, quoteModel).countDocuments({ _id: { $ne: null } })
         res.render('back/index', {
@@ -19,6 +21,7 @@ router.get('/', async (req, res) => {
             contactusCount: contactusCount,
             offerCount: offerCount,
             quoteCount: quoteCount,
+            serviceCount: serviceCount
             message: req.flash('message')
         });
     } else {
