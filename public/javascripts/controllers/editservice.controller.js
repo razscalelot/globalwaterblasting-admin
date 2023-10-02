342c9fc18a324d17f2a7c43ee8275b8dcf384516
razscalelot <you@example.com>
2023-10-02 11:00:02 +0530

    service edit

--- public/javascripts/controllers/editservice.controller.js
+++ public/javascripts/controllers/editservice.controller.js
@@ -206,7 +206,10 @@ app.controller("editServiceController", ($scope, $http, HelperService, $window) 
     };
 
     $scope.updateService = function (editServices) {
-        console.log("editServices", editServices);
+        console.log("editServices.before", editServices.before);
+        console.log("editServices.after", editServices.after);
+        console.log("$scope.service_before", $scope.service_before);
+        console.log("$scope.service_after", $scope.service_after);
         // $http({
         //     url: BASE_URL + 'edit',
         //     method: "POST",
