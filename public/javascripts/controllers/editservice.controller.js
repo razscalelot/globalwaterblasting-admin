f19a06b0e5a2694971c84b7b087122b495ae7128
razscalelot <you@example.com>
2023-10-02 10:35:04 +0530

    service edit

--- public/javascripts/controllers/editservice.controller.js
+++ public/javascripts/controllers/editservice.controller.js
@@ -217,8 +217,8 @@ app.controller("editServiceController", ($scope, $http, HelperService, $window) 
                 banner: (editServices.service_banner == null) ? editServices.banner : editServices.banner,
                 shortdesc: editServices.shortdesc,
                 longdesc: editServices.longdesc,
-                before: (editServices.service_before == null) ? editServices.before : editServices.service_before,
-                after: (editServices.service_after == null) ? editServices.after : editServices.service_after,
+                before: (editServices.service_before == null) ? editServices.images.before : editServices.service_before,
+                after: (editServices.service_after == null) ? editServices.images.after : editServices.service_after,
                 title: editServices.servicedetails.title,
                 longdesc1: editServices.servicedetails.longdesc1,
                 points: editServices.servicedetails.points
